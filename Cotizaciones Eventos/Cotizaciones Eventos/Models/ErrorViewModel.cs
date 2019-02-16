5e315cbb90b3925bdc1ce52d9274c3dd039f7e3c
Javier Torres <javierandrestorresreyes@gmail.com>
2019-02-16 13:30:47 -0500

    Creado proyecto

---
+++ Cotizaciones Eventos/Cotizaciones Eventos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cotizaciones_Eventos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
